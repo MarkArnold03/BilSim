517b583e62d5e31b83ecb26ae4094dfc484bbd8c
Mark Arnold Walusimbi <markanorld0@gmail.com>
2023-06-22 01:08:39 +0200

    Klasslibrary

---
+++ ClassLibrary/Models/CarStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Models
+{
+    public class CarStatus
+    {
+        public Direction Direction { get; set; }
+        public int FuelLevel { get; set; }
+    }
+    public enum Direction
+    {
+        Norrut,
+        Söderut,
+        Västerut,
+        Österut
+    }
+}
