bbc4eeda55773c255b3d9ea1e7fb0dc6028371fa
Mark Arnold Walusimbi <markanorld0@gmail.com>
2023-06-25 20:29:28 +0200

    Direction tests

---
+++ CarSimTests/DirectionTests.cs
@@ -0,0 +1,85 @@
+﻿using BilSim.Services;
+using ClassLibrary.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSimTests
+{
+    [TestClass]
+    public class DirectionTests
+    {
+        private CarService carService;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            // Initialize the carService instance
+            carService = new CarService(new Mock<IDriverService>().Object);
+        }
+
+        [TestMethod]
+        public void TurnRight_ChangesDirectionClockwise()
+        {
+            // Arrange
+            Direction initialDirection = carService.GetCarStatus().Direction;
+
+            // Act
+            carService.TurnRight();
+            Direction updatedDirection = carService.GetCarStatus().Direction;
+
+            // Assert
+            Assert.AreNotEqual(initialDirection, updatedDirection); // Direction changes
+                                             
+        }
+
+        [TestMethod]
+        public void TurnLeft_ChangesDirectionCounterClockwise()
+        {
+            // Arrange
+            Direction initialDirection = carService.GetCarStatus().Direction;
+
+            // Act
+            carService.TurnLeft();
+            Direction updatedDirection = carService.GetCarStatus().Direction;
+
+            // Assert
+            Assert.AreNotEqual(initialDirection, updatedDirection); // Direction changes
+                                                                    
+        }
+
+        [TestMethod]
+        public void MoveForward_ChangesDirectionBasedOnCurrentDirection()
+        {
+            // Arrange
+            Direction initialDirection = carService.GetCarStatus().Direction;
+
+            // Act
+            carService.MoveForward();
+            Direction updatedDirection = carService.GetCarStatus().Direction;
+
+            // Assert
+            Assert.AreEqual(initialDirection, updatedDirection); // Direction remains the same
+                                                                 
+        }
+
+        [TestMethod]
+        public void MoveBackward_ChangesDirectionOppositeToCurrentDirection()
+        {
+            // Arrange
+            Direction initialDirection = carService.GetCarStatus().Direction;
+
+            // Act
+            carService.MoveBackward();
+            Direction updatedDirection = carService.GetCarStatus().Direction;
+
+            // Assert
+            Assert.AreNotEqual(initialDirection, updatedDirection); // Direction changes
+                                                                    
+        }
+    }
+
+}
